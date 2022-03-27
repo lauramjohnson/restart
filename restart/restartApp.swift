927f9fa7fd3771e82650be9f5ded532db16dc648
Laura <lauramjohnson@me.com>
2022-03-27 14:32:51 -0500

    Initial Commit

---
+++ restart/restartApp.swift
@@ -0,0 +1,17 @@
+//
+//  restartApp.swift
+//  restart
+//
+//  Created by Laura Johnson on 3/27/22.
+//
+
+import SwiftUI
+
+@main
+struct restartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

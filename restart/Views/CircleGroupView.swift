4e29bc853d70bee8d712915b95a3d59d9ab79b4c
Laura <lauramjohnson@me.com>
2022-03-28 16:30:46 -0500

    button and animation

---
+++ restart/Views/CircleGroupView.swift
@@ -0,0 +1,34 @@
+//
+//  CircleGroupView.swift
+//  restart
+//
+//  Created by Laura Johnson on 3/27/22.
+//
+
+import SwiftUI
+
+struct CircleGroupView: View {
+    @State var ShapeColor: Color
+    @State var ShapeOpacity: Double
+    
+    var body: some View {
+        ZStack {
+            Circle()
+                .stroke(ShapeColor.opacity(ShapeOpacity), lineWidth: 40)
+                .frame(width: 260, height: 260, alignment: .center)
+            Circle()
+                .stroke(ShapeColor.opacity(ShapeOpacity), lineWidth: 80)
+                .frame(width: 260, height: 260, alignment: .center)
+        } //: Zstack
+    }
+}
+
+struct CircleGroupView_Previews: PreviewProvider {
+    static var previews: some View {
+        ZStack {
+            Color("ColorBlue")
+                .ignoresSafeArea(.all, edges: .all)
+            CircleGroupView(ShapeColor: .white, ShapeOpacity: 0.2)
+        }
+    }
+}
